f9f3d887d4ccd8c7b6b7166da69dc665259ea341
Paul Scarrone <ninjapanzer@gmail.com>
2016-03-01 10:47:52 -0500

    Added files via upload

--- dev_server.js
+++ dev_server.js
@@ -1,12 +1,10 @@
-var connect = require('connect');
-var http = require('http');
+var finalhandler = require('finalhandler')
+var http = require('http')
 var serveStatic = require('serve-static')
-
-var app = connect();
  
 // Serve up public/ftp folder 
-var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']})
-
+var serve = serveStatic('.', {'index': ['index.html', 'index.htm']})
+ 
 // Create server 
 var server = http.createServer(function(req, res){
   var done = finalhandler(req, res)
